250bd3cf8c3c68462e655b2dfe4e64485eac6edf
mariorizki <mariorizki07@gmail.com>
2022-11-28 15:41:40 +0700

    create basic pokeapi call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(0);
+
+  const getPokemon = async () => {
+    const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+    const res = await fetch(`${baseUrl}?offset=${offset}&limit=20`);
+
+    const data = await res.json();
+
+    // console.log(data.results);
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+
+    const results = await Promise.all(promises);
+
+    setPokemon([...pokemon, ...results]);
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, [offset]);
+
+  const onClickMore = () => {
+    setOffset(offset + 50);
+  };
+
+  console.log(pokemon);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemon.map((poke) => (
+        <div key={poke.id}>
+          <img src={poke.sprites.front_default} />
+          <p>{poke.name}</p>
+        </div>
+      ))}
+      <button onClick={onClickMore}>Load more</button>
     </div>
   );
 }
